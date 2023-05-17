6e86cb41d7b60e2f3d6c59756ce4a4da87f57f1f
makkarVITVellore <131434805+makkarVITVellore@users.noreply.github.com>
2023-05-17 17:14:55 +0530

    Added solution - LeetHub

---
+++ Middle of Three - GFG/middle-of-three.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String[] S = read.readLine().split(" ");
+            int A=Integer.parseInt(S[0]);
+            int B=Integer.parseInt(S[1]);
+            int C=Integer.parseInt(S[2]);
+            Solution ob = new Solution();
+            System.out.println(ob.middle(A,B,C));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    int middle(int A, int B, int C)
+    {
+        int max = Math.max(A,Math.max(B,C));
+        
+        if(A==max)
+        {
+            return Math.max(B,C);
+        }
+        else if(B==max)
+        {
+            return Math.max(A,C);
+        }
+        
+        return Math.max(A,B);
+    }
+}
