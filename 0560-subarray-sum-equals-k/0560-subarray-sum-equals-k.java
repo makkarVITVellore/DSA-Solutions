12dd53c1e07adb01fae753e82675549f67d64c10
Bharat Makkar <131434805+makkarVITVellore@users.noreply.github.com>
2025-03-01 16:38:46 +0530

    Time: 22 ms (95.91%), Space: 47.3 MB (6.75%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        // Used to store number of subarrays starting from index zero having particular value of sum.
+        HashMap < Integer, Integer > prevSum = new HashMap<Integer , Integer>();
+        int n = nums.length;
+        int res = 0;
+
+        // To keep track of sum of elements so far.
+        int currsum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // Add current element to sum so far.
+            currsum += nums[i];
+
+            // If current sum is equal to desired sum, then a new subarray is found. So, increase count of subarrays.
+            if (currsum == k) {
+                res += 1;
+            }
+            
+            /*
+                If current sum exceeds given sum by current sum  - sum. 
+                Find number of subarrays having this sum in our map and exclude these subarrays.
+            */
+            if (prevSum.containsKey(currsum - k)) {
+
+                res += prevSum.get(currsum - k);
+            }
+
+            // Add current sum value to count of different values of sum.
+            if(prevSum.containsKey(currsum)) {
+                prevSum.put(currsum, prevSum.get(currsum) + 1);
+            } else {
+                prevSum.put(currsum, 1);
+            }
+        }
+
+        return res;      
+    }
+}
