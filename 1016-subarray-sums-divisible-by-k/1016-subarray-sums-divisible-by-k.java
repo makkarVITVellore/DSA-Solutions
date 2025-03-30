7744b83fd437be99405a8828f9df58831da5a0d7
Bharat Makkar <131434805+makkarVITVellore@users.noreply.github.com>
2025-03-30 18:16:00 +0530

    Time: 24 ms (51.08%), Space: 48.3 MB (39.31%) - LeetHub

---
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) { 
+        //map to store sum%k and its frequency
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum = 0;
+        int count = 0;
+        int rem = 0;
+        map.put(rem,1);
+        int n  =nums.length;
+        for(int i=0;i<n;i++){
+            sum = sum + nums[i];
+            rem = sum%k;
+            if(rem<0){
+                rem = rem + k;
+            }
+
+            if(map.containsKey(rem)){
+                count = count + map.get(rem);
+                int freq = map.get(rem);
+                map.put(rem,freq+1);
+            }else{
+                map.put(rem,1);
+            }
+        }
+
+        return count;
+    }
+}
