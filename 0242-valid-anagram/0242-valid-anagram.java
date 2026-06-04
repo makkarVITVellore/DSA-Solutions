d7ed2887b0eecd5fad06a45cc83e87741b8761b1
Bharat Makkar <131434805+makkarVITVellore@users.noreply.github.com>
2026-06-04 17:35:27 +0530

    Time: 15 ms (25.1%), Space: 46.8 MB (5.02%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int l1 = s.length();
+        int l2 = t.length();
+        if(l1!=l2){
+            return false;
+        }
+        
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<l1;i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
+        }
+        
+        for(int i=0;i<l2;i++){
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i),0)-1);
+        }
+        
+        //check whether all characters of our map have freq=0
+        for(Map.Entry<Character,Integer> entry:map.entrySet()){
+            int freq = entry.getValue();
+            if(freq!=0){
+                return false;
+            }
+        }
+        
+        return true;        
+    }
+}
