1ca56878ea83bfde883c39401d51d1f131d54df0
Bharat Makkar <131434805+makkarVITVellore@users.noreply.github.com>
2026-06-15 15:13:56 +0530

    Time: 1 ms (98.49%), Space: 47.2 MB (87.88%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -4,9 +4,8 @@ class Solution {
         int leftSum = 0;
         int rightSum = 0;
         int n = nums.length;
-        
-        for(int i=0;i<n;i++){
-            rightSum = rightSum + nums[i];
+        for(int num: nums){
+            rightSum += num;
         }
 
         for(int i=0;i<n;i++){
